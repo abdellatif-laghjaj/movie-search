4750b20beb8b8ecd29946bfe0bb67451221b6109
Abdellatif Laghjaj <abdellatif.laghjaj@edu.uiz.ac.ma>
2022-08-07 15:34:47 +0100

    add design

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Hello Vue!',
             url: 'http://www.omdbapi.com/?t=',
             api_key: 'b60d4db',
             search: '',
