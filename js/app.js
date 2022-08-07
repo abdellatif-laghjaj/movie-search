69e97b51d120a3ded9608387e5825ab620e3b4c2
Abdellatif Laghjaj <abdellatif.laghjaj@edu.uiz.ac.ma>
2022-08-07 16:14:13 +0100

    fix some bugs

--- js/app.js
+++ js/app.js
@@ -8,5 +8,25 @@ createApp({
             search: '',
             movie: {},
         }
-    }
+    },
+    methods: {
+        searchMovie() {
+            this.movie = {}
+            fetch(this.url + this.search + '&apikey=' + this.api_key)
+                .then(response => response.json())
+                .then(data => {
+                    this.movie = data;
+                    console.log(this.movie)
+                })
+                .catch(error => console.log(error));
+        },
+        validateSearch() {
+            if (this.search.length > 0) {
+                this.searchMovie()
+            }else{
+                this.movie = {}
+                alert('Please enter a movie title');
+            }
+        }
+    },
 }).mount('#app')
